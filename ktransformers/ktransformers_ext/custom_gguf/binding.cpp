4c151519fce097a62b79c45803da5f6da88b8408
Azure <azuretang@foxmail.com>
2024-07-21 12:40:01 +0000

    add q6k

--- ktransformers/ktransformers_ext/custom_gguf/binding.cpp
+++ ktransformers/ktransformers_ext/custom_gguf/binding.cpp
@@ -12,15 +12,14 @@ int test(){
     return 5;
 }
 
+torch::Tensor dequantize_q6_k(torch::Tensor data, int blk_size, torch::Device device);
 
 PYBIND11_MODULE(cudaops, m) {
     m.def("dequantize_q8_0", &dequantize_q8_0, "Function to dequantize q8_0 data.",
           py::arg("data"), py::arg("blk_size"), py::arg("device"));
-    // m.def("dequantize_q4_k", &dequantize_q4_k, "Function to dequantize q4_k data.",
-    //       py::arg("data"));
-    // m.def("dequantize_q6_k", &dequantize_q6_k, "Function to dequantize q6_k data.",
-    //       py::arg("data"));
+    m.def("dequantize_q6_k", &dequantize_q6_k, "Function to dequantize q6_k data.",
+          py::arg("data"), py::arg("blk_size"), py::arg("device"));
+
     m.def("test", &test, "Function to test.");
-    m.def("compare", &compare,
-        py::arg("data"), py::arg("blk_size"), py::arg("device"), py::arg("cmp_scales"), py::arg("cmp_qs"));
+    
 }
--- ktransformers/ktransformers_ext/custom_gguf/ops.h
+++ ktransformers/ktransformers_ext/custom_gguf/ops.h
@@ -5,6 +5,7 @@
 #include <torch/torch.h>
 
 torch::Tensor dequantize_q8_0(torch::Tensor data, int blk_size, torch::Device device);
+torch::Tensor dequantize_q6_k(torch::Tensor data, int blk_size, torch::Device device);
 // torch::Tensor dequantize_q4_k(torch::Tensor data);
 // torch::Tensor dequantize_q6_k(torch::Tensor data);
-int compare(torch::Tensor data, int blk_size, torch::Device device, torch::Tensor cmp_scales, torch::Tensor cmp_qs);
+// int compare(torch::Tensor data, int blk_size, torch::Device device, torch::Tensor cmp_scales, torch::Tensor cmp_qs);
