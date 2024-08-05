50d1394c7ede23155479043a99fe985577f82ee4
chenht2022 <chenht2022@gmail.com>
2024-08-05 09:42:04 +0000

    Add copyright info

--- ktransformers/ktransformers_ext/operators/llamafile/shared_mem_buffer.h
+++ ktransformers/ktransformers_ext/operators/llamafile/shared_mem_buffer.h
@@ -1,3 +1,12 @@
+/**
+ * @Description  :
+ * @Author       : chenht2022
+ * @Date         : 2024-08-05 04:49:08
+ * @Version      : 1.0.0
+ * @LastEditors  : chenht2022 
+ * @LastEditTime : 2024-08-05 06:36:41
+ * @Copyright (c) 2024 by KVCache.AI, All Rights Reserved.
+ **/
 
 #ifndef CPUINFER_SHAREDMEMBUFFER_H
 #define CPUINFER_SHAREDMEMBUFFER_H
