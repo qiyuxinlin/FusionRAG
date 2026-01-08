add sep run on gpus, add average cut, fix preprocess on empty prefix

ktransformers/util/utils.py
@@ -16,7 +16,7 @@ import string
 import json
 import collections
 import numpy as np
-from ktransformers.util.run_ppr import personalized_pagerank, get_top_tokens
+from ktransformers.util.run_ppr import personalized_pagerank, get_top_tokens, highlight_tokens_compare
 from ktransformers.models.custom_cache import StaticCache
 from ktransformers.util.cuda_graph_runner import CUDAGraphRunner
 from ktransformers.util.textstream import TextStreamer
@@ -807,6 +807,10 @@ def load_kv_and_generate(model, tokenizer, past_key_values, passages,
                 print(f"使用了 {len(active_layers)} 个层: {active_layers}")
                 print(f'select_time: {time.time() - select_time:.3f}s')
 
+        elif reprocess_method == "average":
+            ""
+            k_need_index = torch.tensor(list(range(0, sum(passages_len[:-1]), int(1/rate))))
+
         elif 'DraftModel' in reprocess_method:
             # DraftModel: 用小模型 prefill 获取 attention，指导 token 选择
             select_time = time.time()
@@ -877,6 +881,7 @@ def load_kv_and_generate(model, tokenizer, past_key_values, passages,
 
         # reprocess kv cache and prefill question
         k_need_index = torch.sort(k_need_index)[0].tolist()
+        ## add query itself
         k_need_index.extend(range(sum(passages_len[:-1]),sum(passages_len)))
     else:
         k_need_index = range(sum(passages_len[:-1]),sum(passages_len))
@@ -896,7 +901,7 @@ def load_kv_and_generate(model, tokenizer, past_key_values, passages,
         use_sparse_attention = False
     reprocess_inputs = torch.cat(passages)[k_need_index].unsqueeze(0).to(input_device)
     cache_position = torch.tensor(k_need_index, device=input_device)
-
+    highlight_tokens_compare(k_need_index, passages, tokenizer)
     with torch.no_grad():
         without_attn_value = past_key_values.value_cache[-1].narrow(2,0, sum(passages_len[:-1])).clone()
         inputs_embeds = model.model.embed_tokens(reprocess_inputs).to(input_device)

run_question.py
@@ -237,7 +237,7 @@ class FusionRAGModel:
         document_index = all_document.index(document)
         prefill_len = 0
         system_tokens = self.tokenizer.encode(system_prompt, add_special_tokens=True)
-        irrelevant_tokens = self.tokenizer.encode(LOQUACIOUS_TEXT*30, add_special_tokens=True)
+        irrelevant_tokens = self.tokenizer.encode(" ."*10000, add_special_tokens=True)
         system_tensor = torch.tensor(system_tokens, dtype=torch.long)
         system_len = system_tensor.shape[0]
         for i in range(document_index):
@@ -248,10 +248,11 @@ class FusionRAGModel:
         prefill_space_tensor = torch.tensor(irrelevant_tokens[:prefill_len], dtype=torch.long)
         input_tensor = torch.cat((system_tensor, prefill_space_tensor, doc_tensor)).unsqueeze(0)
         hash_key = hashlib.md5(doc_tensor.cpu().numpy().tobytes()).hexdigest()
-        system_cache_path = f'{self.preprocess_empty_prefix_save_path}/{hash_key}_key.pt'
+        empty_prefix_cache_path = f'{self.preprocess_empty_prefix_save_path}/{hash_key}_key.pt'
         prefill_space_len = prefill_space_tensor.shape[0]
         passage_len = doc_tensor.shape[0]
-        if not os.path.exists(system_cache_path):
+        ## this has to be computed everytime.
+        if True or not os.path.exists(empty_prefix_cache_path):
             print(f"[preprocess_one_document_with_empty] generate for hash={hash_key}")
             prefill_and_save_kv_cache(
                 model=self.model,